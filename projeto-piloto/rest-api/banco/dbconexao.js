f6d324ac6ff74972cc7693c530981e466ee06f34
madsonar <madson33@hotmail.com>
2018-09-17 21:54:07 -0300

    Update dbconexao.js

--- projeto-piloto/rest-api/banco/dbconexao.js
+++ projeto-piloto/rest-api/banco/dbconexao.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var conexao = mysql.createPool({
-    host: 'mysql.techsystemsolucoes.com.br',//'localhost'
-    user: 'techsystemsolu01',//'root',
-    password: 'node4040',//''
-    database: 'techsystemsolu01'//'db_portfolio'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'db_portfolio'
 });
 
 module.exports = conexao;
